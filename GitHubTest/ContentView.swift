409baaa5220c9db88c996b4120947efe4142ca62
Uri <=>
2021-05-29 07:07:55 +0300

    Change to bold

--- GitHubTest/ContentView.swift
+++ GitHubTest/ContentView.swift
@@ -11,7 +11,7 @@ struct ContentView: View {
     var body: some View {
         Text("GitHub Test!")
             .font(.body)
-            .fontWeight(.heavy)
+            .fontWeight(.bold)
             .padding()
     }
 }
