3e92a9015978f5c35f535948b15dfba3784ccca1
Uri <=>
2021-05-29 07:04:29 +0300

    my commit message

--- GitHubTest/ContentView.swift
+++ GitHubTest/ContentView.swift
@@ -9,13 +9,17 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        Text("GitHub Test!")
+            .font(.body)
+            .fontWeight(.heavy)
             .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+        }
     }
 }
