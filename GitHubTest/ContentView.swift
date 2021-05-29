6fe139b1380f755e4470b3c7cbcb5ce3cc16c808
uri paz <uripaz9@gmail.com>
2021-05-29 07:11:56 +0300

    new text

--- GitHubTest/ContentView.swift
+++ GitHubTest/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("GitHub Test!")
+        Text("new text!")
             .font(.body)
             .fontWeight(.bold)
             .padding()
